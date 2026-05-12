a16698f21c8f3ba307051fd28f026d9edd7549af
felipesq0710 <felipeschneider0710@gmail.com>
2026-05-12 19:23:52 -0300

    fix: ensure vite dependency

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://127.0.0.1:3001",
         changeOrigin: true,
       },
     },
   },
-})
+});
